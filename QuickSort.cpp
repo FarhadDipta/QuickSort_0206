5b85dfa377a0a474f05fa69f766c3e6a746f26a9
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-03-28 14:21:05 +0700

    step 12

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -77,8 +77,12 @@ void q_short (int low, int high)
     }
     
     if (low < j){ // step 11
-
+        //swap the pivot element with the element at index j
+        swap (low, j);
     }
+
+    //recursive call to sort the left sub array
+    q_short(low, j -1); //step 12
 }
 
 int main(){
